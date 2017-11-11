a95798624e519e73c186710aadd95f4659652e54
Suha Park <suhapark@Suhas-MacBook-Pro.local>
2017-11-11 11:23:27 -0800

    added few arrays

--- hotServer.js
+++ hotServer.js
@@ -10,3 +10,30 @@ app.use(bodyParser.json());
 app.listen(PORT, function() {
 	console.log("Listening at PORT: " + PORT);
 })
+
+var tableSection
+
+//Grabs customer input
+var name
+var phoneNum
+var email
+var uniqueId 
+
+//Creates local "temporary" object for holding data
+var newCustomer = {
+	name: name,
+	phoneNum: phoneNum,
+	email: email,
+	uniqueId: uniqueId
+};
+
+//Check to make sure it works
+console.log(newCustomer.name);
+console.log(newCustomer.phoneNum);
+console.log(newCustomer.email);
+console.log(newCustomer.uniqueId);
+
+//Store everything into a varaiable
+var waitinglistSection
+var table
+
